2ade449a15f082ae92a24d62bbe0e265153d887c
Ben Sigelman <bhs@resonancelabs.com>
2016-07-12 16:08:50 -0700

    Use the referee nomenclature

--- src/reference.js
+++ src/reference.js
@@ -23,8 +23,8 @@ export default class Reference {
      * @return {SpanContext} The SpanContext being referred to (e.g., the
      *         parent in a REFERENCE_CHILD_OF Reference).
      */
-    spanContext() {
-        return this._spanContext;
+    referee() {
+        return this._referee;
     }
 
     /**
@@ -32,20 +32,20 @@ export default class Reference {
      *
      * @param {string} type - the Reference type constant (e.g.,
      *        REFERENCE_CHILD_OF or REFERENCE_FOLLOWS_FROM).
-     * @param {SpanContext} spanContext - the SpanContext being referred to. As
+     * @param {SpanContext} referee - the SpanContext being referred to. As
      *        a convenience, a Span instance may be passed in instead (in which
      *        case its .context() is used here).
      */
-    constructor(type, spanContext) {
+    constructor(type, referee) {
         if (API_CONFORMANCE_CHECKS) {
-            if (!(spanContext instanceof SpanContext || spanContext instanceof Span)) {
-                throw new Error('spanContext must be a Span or SpanContext instance');
+            if (!(referee instanceof SpanContext || referee instanceof Span)) {
+                throw new Error('referee must be a Span or SpanContext instance');
             }
         }
         this._type = type;
-        this._spanContext = (
-                spanContext instanceof Span ?
-                spanContext.context() :
-                spanContext);
+        this._referee = (
+                referee instanceof Span ?
+                referee.context() :
+                referee);
     }
 }
