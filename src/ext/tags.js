344ea29df2a45f8c50406c3c5b19d1458b9b2b85
bcronin <bcronin@lightstep.com>
2016-08-22 16:21:41 -0700

    Remove copyright notice from source file

--- src/ext/tags.js
+++ src/ext/tags.js
@@ -1,30 +1,3 @@
-// Copyright (c) 2016 Resonance Labs, Inc
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-// Here we define standard names for tags that can be added to spans by the
-// instrumentation code. The actual tracing systems are not required to
-// retain these as tags in the stored spans if they have other means of
-// representing the same data. For example, the SPAN_KIND:'server' can be
-// inferred from a Zipkin span by the presence of ss/sr annotations.
-// For more information visit http://opentracing.io/data-semantics/
-
 module.exports = {
 
     /** ------------------------------------------------------------------------
