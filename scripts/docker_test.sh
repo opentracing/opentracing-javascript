6d7bf5536656bc4f9f546e7684d047fc1e6b98aa
Felix Becker <felix.b@outlook.com>
2017-04-12 22:54:12 +0200

    Change npm test script to test without coverage

--- scripts/docker_test.sh
+++ scripts/docker_test.sh
@@ -2,4 +2,4 @@
 
 docker run -t --rm --name test-runner-opentracing-javascript \
     -v ${PWD}:/usr/src/opentracing -w /usr/src/opentracing \
-    node:$1 make test-no-coverage
+    node:$1 npm test
