bde00af8a8b82471ea7d2d78e6c155ffc88b7838
Felix Becker <felix.b@outlook.com>
2017-04-14 15:04:08 +0200

    Remove debug entry point

--- debug.js
+++
@@ -1,3 +0,0 @@
-// The main lib now contains all the type checks lib-debug had previously by default
-process.emitWarning('The opentracing/debug entry point is deprecated', 'DeprecationWarning');
-module.exports = require('./lib');
