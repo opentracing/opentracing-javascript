4ccfd017ca6b5cb7cf7020ace4f7cc00268b708b
Yuri Shkuro <ys@uber.com>
2017-11-30 11:07:01 -0500

    Fix linter errors
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- src/mock_tracer/mock_context.ts
+++ src/mock_tracer/mock_context.ts
@@ -1,4 +1,5 @@
 import * as opentracing from '../index';
+import MockSpan from './mock_span';
 
 /**
  * OpenTracing Context implementation designed for use in
@@ -10,13 +11,17 @@ export class MockContext extends opentracing.SpanContext {
     // MockContext-specific
     //------------------------------------------------------------------------//
 
-    private _span: opentracing.Span;
+    private _span: MockSpan;
 
-    constructor(span: opentracing.Span) {
+    constructor(span: MockSpan) {
         super();
         // Store a reference to the span itself since this is a mock tracer
         // intended to make debugging and unit testing easier.
         this._span = span;
+    }
+
+    span(): MockSpan {
+        return this._span;
     }
 }
 
