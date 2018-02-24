c19f4ee91b6b6c45814a5f8d627766780bb96a5c
Alois Reitbauer <alois@reitbauer.cc>
2018-02-24 12:54:02 +0100

    added mock tracer tests

--- src/test/unittest.ts
+++ src/test/unittest.ts
@@ -2,8 +2,12 @@
 require('source-map-support').install();
 
 import apiCompatibilityChecks from './api_compatibility';
+import mocktracerImplementationTests from './mocktracer_implemenation';
 import noopImplementationTests from './noop_implementation';
 import opentracingAPITests from './opentracing_api';
+
+// basic tests for the mock tracer
+mocktracerImplementationTests ();
 
 // Run the tests on the default OpenTracing no-op Tracer.
 noopImplementationTests();
@@ -13,3 +17,4 @@ apiCompatibilityChecks();
 
 // Basic unittests for opentracing
 opentracingAPITests();
+
