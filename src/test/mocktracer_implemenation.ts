1d8f638897186fac72217b9b307affb2081b8bc8
Alois Reitbauer <alois@reitbauer.cc>
2018-02-24 14:45:42 +0100

    fix

--- src/test/mocktracer_implemenation.ts
+++ src/test/mocktracer_implemenation.ts
@@ -14,7 +14,7 @@ export function mockTracerimplementationTests(createTracer = () => new MockTrace
                 span.setTag('tag name', 'tag value');
                 span.log({state: 'test'});
                 const mockSpan = span as MockSpan;
-                span.debug();
+                mockSpan.debug();
                 expect(() => { span.finish(); }).to.not.throw (Error);
                 // currently injection is not implemented
                 // const textCarrier = {};
