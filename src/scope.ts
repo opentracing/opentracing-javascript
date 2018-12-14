2f29b13a42d402f589f267905dbac8f4cacb25c6
rochdev <roch.devost@datadoghq.com>
2018-12-14 14:39:14 -0500

    update bind to support more types

--- src/scope.ts
+++ src/scope.ts
@@ -1,6 +1,29 @@
+import wrapEmitter from 'emitter-listener';
+import * as shimmer from 'shimmer';
 import Span from './span';
 
 export type ScopeCallback<T> = (...args: any[]) => T | void;
+
+export type EventName = string | symbol;
+
+export type EventListener = (...args: any[]) => any;
+
+export interface Markable {
+    [key: string]: (Span | null)[];
+}
+
+export type EmitterMethod = (eventName: EventName, listener: EventListener) => any;
+
+export interface EventEmitter {
+    emit(eventName: EventName, ...args: any[]): any;
+    on: EmitterMethod;
+    addListener: EmitterMethod;
+    removeListener: EmitterMethod;
+}
+
+export type BindTarget<T> = ScopeCallback<T> | EventEmitter | Promise<T>;
+
+export const SCOPE_SYMBOL = 'opentracing@scope';
 
 /**
  * Scope manages context propagation of a Span.
@@ -27,7 +50,7 @@ export class Scope {
      * @param {ScopeCallback<T>} callback
      *        The callback for which to activate the span.
      *
-     * @returns {T} The return value of the provided callback.
+     * @returns {T | void} The return value of the provided callback.
      */
     activate<T>(span: Span | null, callback: ScopeCallback<T>): T | void {
         if (typeof callback !== 'function') { return callback; }
@@ -36,41 +59,106 @@ export class Scope {
     }
 
     /**
-     * Binds the target function to the provided span, or the current active
-     * span otherwise.
+     * Binds the target to the provided span, or the active span otherwise.
+     * For promises and event emitters, the active span is evaluated when a
+     * handler is registered.
      *
-     * @param {ScopeCallback<T>} fn
+     * @param {any} target
      * @param {Span | null} [span]
      *
-     * @returns {ScopeCallback<T>} The provided function bound to the span.
+     * @returns {any} The provided function bound to the span.
      */
-    bind<T>(fn: ScopeCallback<T>, span?: Span | null): ScopeCallback<T> {
-        if (typeof fn !== 'function') { return fn; }
-
-        if (span === undefined) {
-          span = this.active();
+    bind<T>(fn: ScopeCallback<T>, span?: Span | null): ScopeCallback<T>;
+    bind<T>(fn: Promise<T>, span?: Span | null): Promise<T>;
+    bind(emitter: EventEmitter, span?: Span | null): EventEmitter;
+    bind(target: any, span?: Span | null, ...args: any[]): any {
+        if (this._isEmitter(target)) {
+            return this._bindEmitter(target, span);
+        } else if (this._isPromise(target)) {
+            return this._bindPromise(target, span);
+        } else if (typeof target === 'function') {
+            return this._bindFn(target, span);
+        } else {
+            return this._bind(target, span, ...args);
         }
-
-        return this._bindFn(fn, span);
     }
 
-    // By default does nothing
     protected _active(): Span | null {
         return null;
     }
 
-    // By default does nothing
     protected _activate<T>(span: Span | null, callback: () => T): T {
         return callback();
     }
 
-    private _bindFn<T>(fn: ScopeCallback<T>, span?: Span | null): ScopeCallback<T> {
-        return function (...args: any[]): T {
-            const self = this;
+    protected _bind<T>(target: T, span?: Span | null, ...args: any[]): T {
+        return target;
+    }
 
-            return this.activate(span, () => {
-                return fn.apply(self, args);
+    protected _bindFn<T>(fn: ScopeCallback<T>, span?: Span | null): ScopeCallback<T> {
+        const scope = this;
+        const spanOrActive = this._spanOrActive(span);
+
+        return function (...args: any[]): T {
+            return scope.activate(spanOrActive, () => {
+                return fn.apply(this, args);
             });
         };
     }
+
+    protected _bindEmitter(emitter: EventEmitter, span?: Span | null): EventEmitter {
+        const scope = this;
+
+        function mark(listener: EventListener & Markable): void {
+            if (!listener) { return; }
+
+            listener[SCOPE_SYMBOL] = listener[SCOPE_SYMBOL] || [];
+            listener[SCOPE_SYMBOL].push(scope._spanOrActive(span));
+        }
+
+        function prepare(listener: EventListener & Markable): EventListener {
+            if (!listener || !listener[SCOPE_SYMBOL]) { return listener; }
+
+            return listener[SCOPE_SYMBOL]
+                .reduce((prev: EventListener, next: Span | null): EventListener => {
+                    return scope.bind(prev, next);
+                }, listener);
+        }
+
+        wrapEmitter(emitter, mark, prepare);
+
+        return emitter;
+    }
+
+    protected _bindPromise<T>(promise: Promise<T>, span?: Span | null): Promise<T> {
+        const scope = this;
+
+        shimmer.wrap(promise, 'then', (then: any): any => {
+            return function (...args: any[]): Promise<T> {
+                return then.apply(this, args.map(arg => {
+                    if (typeof arg !== 'function') { return arg; }
+                    return scope.bind(arg, span);
+                }));
+            };
+        });
+
+        return promise;
+    }
+
+    private _spanOrActive(span?: Span | null): Span | null {
+        return span !== undefined ? span : this.active();
+    }
+
+    private _isEmitter(emitter: EventEmitter): boolean {
+        return emitter &&
+            typeof emitter.emit === 'function' &&
+            typeof emitter.on === 'function' &&
+            typeof emitter.addListener === 'function' &&
+            typeof emitter.removeListener === 'function';
+    }
+
+    private _isPromise<T>(promise: Promise<T>): boolean {
+        return promise && typeof promise.then === 'function';
+    }
 }
+Object.create(null).ptoto;
